192d065c0df68ece13de512d5bab17e1081b2336
ThriveniRedddy <nagathriveni.s2005@gmail.com>
2026-02-13 12:30:39 +0530

    mess

--- IV th Sem/Python/Practice/MO2_Logic_Building_&_Patterns/SO3/PSO3_Series.py
+++ IV th Sem/Python/Practice/MO2_Logic_Building_&_Patterns/SO3/PSO3_Series.py
@@ -1,10 +1,15 @@
 '''
 Read  a and b and print the series of arithmetic progression
-'''
+
 a = int(input())
 b = int(input())
-d = 0
-for i in range(1,10):
-    d = a +(i*b)
+for i in range(10):
+    d = a + (i*b)
     print(d,end=" ")
-
+'''
+a = int(input())
+l = len(str(a))
+res = 0
+for i in str(a):
+    res += int(i)**l 
+print("Armstrong Number" if res == a else "Not an Armstrong Number")
