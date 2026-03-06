b03610b532b4cae33a0600778b1800b3b13a488d
ThriveniRedddy <nagathriveni.s2005@gmail.com>
2026-03-06 19:54:51 +0530

    msg

--- IV th Sem/Python/Practice/MO2_Logic_Building_&_Patterns/S05/PS06_Intermediate_patterns.py
+++ IV th Sem/Python/Practice/MO2_Logic_Building_&_Patterns/S05/PS06_Intermediate_patterns.py
@@ -16,13 +16,32 @@ Output:
   * * *
 * *  *  *
 '''
+'''
 n = int(input())
 for i in range(1,n+1):
-    print(" "*(n-i)+"* "*i)
+    print(" "*(n-i)+"* "*i)'''
 # * * * *
 # * * *
 #  * *
 #   *
+''' 3.Diamond
+   *
+  *  *
+*  *  * 
+  *  *
+    *
+'''
+''''
 n = int(input())
 for i in range(1,n+1):
+    print(" "*(n-i)+"* "*i)
+for i in range(n-1,0,-1):
+    print(" "*(n-i)+"* "*i)'''
+# Pyramid with Numbers
+n = int(input())
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end = "")
+    for k in range(1,i+1):
+        print(k,end = " ")
     print()
